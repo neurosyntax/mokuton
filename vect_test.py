b7eb27effd64318c501088509689023b6e75b9d7
levelicarus <levelicarus@gmail.com>
2017-03-10 23:31:37 -0500

    Found and fixed an issue where literals like 0L, long numerics in Java, are treated as unicode by the Python javalang module, so we can't determine the actual type. The script marks this ast as malformed and does not add it to the db, and does not vectorize it. If allowed to vectorize, the javalang module gives 'unicode' as the type and that does not appear in the nodeVect dictionary of vectore feature tokens, so throws a KeyError.

--- vect_test.py
+++ vect_test.py
@@ -20,8 +20,11 @@ import collections
 from ast import nodes
 from ast import nodeVect
 
+# well-formed
 # code = 'public class HelloWorld{public static int findFirst(int value, int idx) { value &= ~((1 << idx) - 1); int result = Integer.numberOfTrailingZeros(value);        return (result == 32) ? -1 : result;}}'
-code = 'public class HelloWorld{public static float add(int a, int b){a+=5; return 3.14;}}'
+# code = 'public class HelloWorld{public static float add(int a, int b){a+=5; return 3.14;}}'
+# malformed from unicode e.g. 0L and \"
+code = 'public class HelloWorld{public static long verifyPositive(long value, String paramName) {        if (value <= 0L) {            throw new IllegalArgumentException(paramName + \" > 0 required but it was \" + value);        }        return value;    }}'
 tree = javalang.parse.parse(code)
 
 # Convert string to numeric
@@ -37,6 +40,9 @@ def num(s):
 def getLiteral(vals):
 	for v in vals:
 		if isinstance(v, basestring):
+			if not(type(num(v)).__name__.strip in nodeVect):
+				global malformed
+				malformed = True
 			return type(num(v)).__name__
 
 def generateAST(tree):
@@ -46,7 +52,6 @@ def generateAST(tree):
 		sub.append(str(getLiteral(tree.children)))
 	else:
 		sub.append(str('('))
-		# sub.append(str(nodeVect[str(tree)]))
 		sub.append(str(tree))
 	leaves = ''
 	for n in tree.children:
@@ -56,7 +61,6 @@ def generateAST(tree):
 		elif str(n) in nodes:
 			sub.append(generateAST(n))
 	sub.append(leaves.strip())
-	# sub.append(str(nodeVect[')']))
 	sub.append(str(')'))
 	return sub
 
@@ -69,11 +73,19 @@ def flatten(l):
 			yield el
 
 def vectorize(tree):
+	print tree
 	for i, t in enumerate(tree):
-		tree[i] = nodeVect[t]
+		tree[i] = nodeVect[str(t)]
 	return tree
 
 if __name__ == "__main__":
+	global malformed
+	malformed = False
 	# This bit assumes the tree returned contains  
 	vec = list(flatten(generateAST(tree)))
-	print vectorize([v for v in vec if len(v) > 0][4:][:-2])
+	print 'malformed: ', malformed
+	if not malformed:
+		print vectorize([v for v in vec if len(v) > 0][4:][:-2])
+	else:
+		print 'AST malformed'
+		print vec
